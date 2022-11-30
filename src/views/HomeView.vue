9c1382eb6be19f4ffc19072a0e2eaf222ebf339b
jSuttipong <senasuttiphan.s@gmail.com>
2022-11-30 21:56:35 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Home from '../components/Home.vue'
+import { ref, onMounted } from 'vue'
+
+const el = ref()
+
+onMounted(() => {
+  el.value // <div>
+  console.log('el.value =>',el.value);
+})
+
+</script>
+
+<template>
+  <main>
+    <Home />
+    <div ref="el"></div>
+    {{msg}}
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello world!'
+    }
+  },
+  mounted(){
+    console.log('this mounted');
+  }
+}
+</script>
